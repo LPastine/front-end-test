1e65d8c7fb709ef6f49955a9fe28c4f0e332713b
LPastine <lucianopastine.tech@gmail.com>
2024-11-06 19:23:34 -0300

    add footer styles

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,10 +1,48 @@
 import styles from "./Footer.module.css";
+import BrandLogo from "./icons/BrandLogo";
+import ContactIcon from "./icons/ContactIcon";
+import MailIcon from "./icons/MailIcon";
 
 const Footer = () => {
   return (
-    <div className={`${styles.Footer}`}>
-      <div>Footer</div>
-    </div>
+    <footer className={`${styles.Footer}`}>
+      <div className={`${styles.Footer__container}`}>
+        <div className={`${styles.Footer__top}`}>
+          <div className={`${styles.Footer__contactButtons}`}>
+            <button className={`${styles.Footer__contactButton}`}>
+              <div className={`${styles.Footer__contactButton__icon}`}>
+                <MailIcon />
+              </div>
+              ENTRE EM CONTATO
+            </button>
+            <button className={`${styles.Footer__contactButton}`}>
+              <div className={`${styles.Footer__contactButton__icon}`}>
+                <ContactIcon />
+              </div>
+              FALE COM O NOSSO CONSULTOR ONLINE
+            </button>
+          </div>
+        </div>
+        <div className={`${styles.Footer__bottom}`}>
+          <div className={`${styles.Footer__localization}`}>
+            <p className={`${styles.Footer__localization__title}`}>
+              Localização
+            </p>
+            <p className={`${styles.Footer__localization__text}`}>
+              Avenida Andrômeda, 2000. Bloco 6 e 8 - Alphavile SP
+              <br />
+              brasil@corebiz.ag
+              <br />
+              +55 11 3090 1039
+            </p>
+          </div>
+          <div className={`${styles.Footer__brand}`}>
+            <p className={`${styles.Footer__brand__text}`}>Created by</p>
+            <BrandLogo />
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
