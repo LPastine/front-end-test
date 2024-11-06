a1516eae4b2bfe2930a94a72f565a8d8b524c1a2
LPastine <lucianopastine.tech@gmail.com>
2024-11-06 19:38:01 -0300

    add footer responsiveness

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,28 +1,33 @@
+import useDevice from "../../../hooks/useDevice";
 import styles from "./Footer.module.css";
 import BrandLogo from "./icons/BrandLogo";
 import ContactIcon from "./icons/ContactIcon";
 import MailIcon from "./icons/MailIcon";
 
 const Footer = () => {
+  const isMobile = useDevice();
+
   return (
     <footer className={`${styles.Footer}`}>
       <div className={`${styles.Footer__container}`}>
-        <div className={`${styles.Footer__top}`}>
-          <div className={`${styles.Footer__contactButtons}`}>
-            <button className={`${styles.Footer__contactButton}`}>
-              <div className={`${styles.Footer__contactButton__icon}`}>
-                <MailIcon />
-              </div>
-              ENTRE EM CONTATO
-            </button>
-            <button className={`${styles.Footer__contactButton}`}>
-              <div className={`${styles.Footer__contactButton__icon}`}>
-                <ContactIcon />
-              </div>
-              FALE COM O NOSSO CONSULTOR ONLINE
-            </button>
+        {!isMobile && (
+          <div className={`${styles.Footer__top}`}>
+            <div className={`${styles.Footer__contactButtons}`}>
+              <button className={`${styles.Footer__contactButton}`}>
+                <div className={`${styles.Footer__contactButton__icon}`}>
+                  <MailIcon />
+                </div>
+                ENTRE EM CONTATO
+              </button>
+              <button className={`${styles.Footer__contactButton}`}>
+                <div className={`${styles.Footer__contactButton__icon}`}>
+                  <ContactIcon />
+                </div>
+                FALE COM O NOSSO CONSULTOR ONLINE
+              </button>
+            </div>
           </div>
-        </div>
+        )}
         <div className={`${styles.Footer__bottom}`}>
           <div className={`${styles.Footer__localization}`}>
             <p className={`${styles.Footer__localization__title}`}>
@@ -36,6 +41,22 @@ const Footer = () => {
               +55 11 3090 1039
             </p>
           </div>
+          {isMobile && (
+            <div className={`${styles.Footer__contactButtons}`}>
+              <button className={`${styles.Footer__contactButton}`}>
+                <div className={`${styles.Footer__contactButton__icon}`}>
+                  <MailIcon />
+                </div>
+                ENTRE EM CONTATO
+              </button>
+              <button className={`${styles.Footer__contactButton}`}>
+                <div className={`${styles.Footer__contactButton__icon}`}>
+                  <ContactIcon />
+                </div>
+                FALE COM O NOSSO CONSULTOR ONLINE
+              </button>
+            </div>
+          )}
           <div className={`${styles.Footer__brand}`}>
             <p className={`${styles.Footer__brand__text}`}>Created by</p>
             <BrandLogo />
